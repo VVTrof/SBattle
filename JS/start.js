835e752283b5717d9229516c1568e14be72ea399
VVTrof <37374627+VVTrof@users.noreply.github.com>
2018-04-09 02:39:17 +0300

    ..

--- JS/start.js
+++ JS/start.js
@@ -1,11 +1,3 @@
+'use strict'
 import { coverPage } from './coverPage.js';
-// выбранный уровень
-// var lvl = 0;
-// выбранная ПЛ
-// var nSub = 0;
-console.log('Запуск приложения');
-// loadingPicture.onload = function () {
-// document.getElementById('loadingPicture').onload = null;
-//  console.log('продолжаем загрузку приложения');
 coverPage()
-// };
